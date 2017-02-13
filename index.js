c30c9e0806b09b6738721114aba7e4562fbbe3cd
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-02-13 12:57:28 -0200

    calls init after being instiated

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const NGF = require('./lib')
 
 module.exports = function(opts) {
-  new NGF(require('child_process'), /^win/.test(process.platform) ? 'yo.cmd' : 'yo', opts)
+  new NGF(
+      require('child_process'),
+      /^win/.test(process.platform) ? 'yo.cmd' : 'yo',
+      opts
+    )
+    .init()
 }
